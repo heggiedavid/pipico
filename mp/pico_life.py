f174420b5f48399310d54b4c4390251cdcab845d
David Heggie <david.heggie@gmail.com>
2021-08-16 21:31:49 +0100

    Some useful comments

--- mp/pico_life.py
+++ mp/pico_life.py
@@ -6,10 +6,12 @@
 # TODO: Make rulesets configurable
 # TODO: Make sure it's actually working properly ...
 
+# Uses Pimoroni's Pico Unicorn pack - needs the pimoroni UF2
 import picounicorn
 import random
 import time
 
+# unicorn pack is 16 x 7
 grid_w = 16
 grid_h = 7
 
@@ -18,8 +20,8 @@ picounicorn.init()
 # initialise random grid and empty next grid
 grid = [[random.randint(0, 1) for col in range(grid_w)] for row in range(grid_h)]
 next_grid = [[0 for col in range(grid_w)] for row in range(grid_h)]
-    
-#update grid
+
+#update grid with rules for next generation
 def next_generation(grid, next_grid):
     for row in range(grid_h):
         for col in range(grid_w):
@@ -28,6 +30,7 @@ def next_generation(grid, next_grid):
             for i in range(-1, 2):
                 for j in range(-1, 2):
                     if not (i == 0 and j == 0):
+                        # uses modulo to wrap around the grid
                         live_neighbours += grid[((row + i) % grid_h)][((col + j) % grid_w)]          
             
             # process rules
@@ -46,11 +49,13 @@ def display_grid(grid, picounicorn, w, h):
             if grid[y][x] == 0:
                 picounicorn.set_pixel(x, y, 0, 0, 0)
             else:
+                # a nice shade of blue
                 picounicorn.set_pixel(x, y, 100, 100, 150)
 
 display_grid(grid, picounicorn, grid_w, grid_h)
 next_grid = next_generation(grid, next_grid)
 
+# TODO: don't have this in an infinite loop - run for a few generations then start again
 while True:
     time.sleep_ms(100)
     display_grid(next_grid, picounicorn, grid_w, grid_h)
